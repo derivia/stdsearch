f0850feb68be385780fe7bb540321b25d4bcb012
derivia <contato.lorransoares@gmail.com>
2024-10-12 23:28:41 -0300

    refactor: faster search using tab event listener
    the service worker now starts running when a new tab is created
    instead of waiting for the user to enter something to search

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
+// makes the service worker load when a new tab is created
+chrome.tabs.onCreated.addListener(() => {});
+
 chrome.omnibox.onInputEntered.addListener((text) => {
   const searchUrl = `https://doc.rust-lang.org/std/?search=${encodeURIComponent(text)}`;
   chrome.tabs.update({ url: searchUrl });
