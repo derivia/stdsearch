56d38cfdc99c520d21dd3a48001f51c679c3f961
derivia <contato.lorransoares@gmail.com>
2024-10-14 21:03:00 -0300

    feat!: added two options - std & crate
    new usage:
    1. `!rust` (to enter omnibox)
    2. them one of the two options: std | crate
    
    (std goes to rust-lang std documentation and crate goes to docs.rs)

--- background.js
+++ background.js
@@ -1,7 +1,40 @@
-// makes the service worker load when a new tab is created
-chrome.tabs.onCreated.addListener(() => {});
-
 chrome.omnibox.onInputEntered.addListener((text) => {
-  const searchUrl = `https://doc.rust-lang.org/std/?search=${encodeURIComponent(text)}`;
-  chrome.tabs.update({ url: searchUrl });
+  const [type, ...args] = text.split(" ");
+  const query = args.join(" ");
+  const searchUrl = handleTextEntered(type, query);
+  if (searchUrl) {
+    chrome.tabs.update({ url: searchUrl });
+  }
 });
+
+function handleTextEntered(type, query) {
+  let result = "";
+  let search = "";
+
+  switch (type) {
+    case "std":
+      search = encodeURIComponent(query || "");
+
+      result = `https://doc.rust-lang.org/stable/std/`.concat(
+        search ? `?search=${search}` : "",
+      );
+      return result;
+
+    case "crate":
+      const [crateName, ...searchTerm] = query.split(" ");
+      let crate = encodeURIComponent(crateName || "");
+
+      if (!crate) {
+        return `https://docs.rs/`;
+      }
+
+      search = encodeURIComponent(searchTerm.join(" ") || "");
+
+      result = `https://docs.rs/${crate}/latest/${crate}/`.concat(
+        search ? `?search=${search}` : "",
+      );
+      return result;
+    default:
+      return null;
+  }
+}
